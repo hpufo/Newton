43a670a2cd1e5d6a03716f6ff6ebb5b2bc175caa
Christopher Stephenson <hpufo2@gmail.com>
2017-10-24 00:00:16 -0400

    Using functional stateless components instead of a class component

--- src/js/components/ResultTitle.js
+++ src/js/components/ResultTitle.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-export default class ResultTitle extends React.Component {
-  render(){
-    return (
-      <li>{this.props.title}</li>
-    );
-  }
+const ResultTitle = (props) => {
+  return <li>{props.title}</li>;
 }
+
+export default ResultTitle;
